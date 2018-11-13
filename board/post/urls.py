43c4fcfdeb719f4d9e924b1581c80c1b0b08cd96
sakura <element.kgo@gmail.com>
2018-11-13 02:09:14 +0000

    create 완성

--- board/post/urls.py
+++ board/post/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
+# url 요청 작업시 실수를 줄이기 위해 사용 - 변수를 만들어 변수명을 주소대신 사용
+app_name = 'post'
+
 urlpatterns = [
-    path('', views.list)
+    path('', views.list, name='list'),
+    path('create/', views.create, name='create')
     
     ]
--- board/post/views.py
+++ board/post/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
+from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 def list(request):
     return render(request, 'post/list.html')
+    
+def create(request):
+    if request.method == 'POST':
+        # 저장로직
+        # title, content 에 적었던 내용을 받아옮
+        form = PostForm(request.POST)
+        
+        # 입력 규칙 설정
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            # title = form.cleand_data.get('title')
+            content = form.cleaned_data['content']
+            Post.objects.create(title=title, content=content)    
+            return redirect(resolve_url('post:list'))
+            
+    else:
+        # 사용자에게 입력할 수 있는 폼 리턴
+        form = PostForm()
+    return render(request, 'post/create.html', {'form': form})
